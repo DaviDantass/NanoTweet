f3beaacb9cc445ece86cfca94208794d39f558f2
Davi Dantas <davidantasdev@gmail.com>
2025-12-07 15:14:45 -0300

    first commit

---
+++ src/main/java/com/davidantasdev/NanoTweet/NanoTweetApplication.java
@@ -0,0 +1,11 @@
+package com.davidantasdev.NanoTweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoTweetApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NanoTweetApplication.class, args);
+	}
+}
